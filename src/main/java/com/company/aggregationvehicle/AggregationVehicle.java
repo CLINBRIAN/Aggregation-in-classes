d9a63d35a198071bb5edd0351b5683f87fafac1a
kamau <kamau@DESKTOP-MRLMA36>
2024-06-21 23:45:18 +0300

    Aggregation

---
+++ src/main/java/com/company/aggregationvehicle/AggregationVehicle.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.company.aggregationvehicle;
+
+/**
+ *
+ * @author kamau
+ */
+class Vehicle{
+   private String vin;
+
+   public String getVin() {
+      return vin;
+   }
+
+   public void setVin(String vin) {
+      this.vin = vin;
+   }	
+}
+class Speed{
+   private double max;
+
+   public double getMax() {
+      return max;
+   }
+
+   public void setMax(double max) {
+      this.max = max;
+   }
+}
+
+class Van extends Vehicle {
+   private Speed speed;
+
+   public Speed getSpeed() {
+      return speed;
+   }
+
+   public void setSpeed(Speed speed) {
+      this.speed = speed;
+   }
+
+   public void print() {
+      System.out.println("Vin: " +this.getVin() + ", Max Speed: " + speed.getMax() );
+   }
+} 
+
+public class AggregationVehicle {
+
+    public static void main(String[] args) {
+        Speed speed = new Speed();
+      speed.setMax(120);
+      Van van = new Van();
+
+      van.setVin("abcd1233");
+      van.setSpeed(speed);
+      van.print();
+    }
+}
